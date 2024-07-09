3e2f5444920da3894a5cf2159fc8f89e950a9a7a
Anna Eames Seffernick <seffernick.13@buckeyemail.osu.edu>
2024-07-09 12:42:10 -0500

    Updated beam_specs_sm documentation

--- R/beam_specs_sm.R
+++ R/beam_specs_sm.R
@@ -3,7 +3,7 @@
 #' The small beam.specs object used in example compute_beam_stats function.
 #'
 #' @format ## `beam_specs_sm`
-#' A data frame with 4 rows and 3 columns:
+#' A data frame with 2 rows and 3 columns:
 #' \describe{
 #'   \item{name}{Analysis name with omic and endpoint}
 #'   \item{mtx}{Name of omics matrix used in the analysis}
